7029fa41002a4b578a090a4d94a9ca59c8292a5b
Kumenit <86913518+Kumenit@users.noreply.github.com>
2022-04-01 10:02:34 -0700

    cg

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,55 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-
 /**
- * main - prints the minimum number of coins
- * to make change for an amount of money
- * @argc: n args
- * @argv: arr args
- * Return: 0
+ *main - prints minimum amount of coins to give
+ *
+ *@argc:arguement count
+ *@argv:arguement vector
+ *
+ *Return:0(success)
+ *
  */
 int main(int argc, char *argv[])
 {
-	int value, c;
+	int cents, change;
 
-	c = 0;
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	value = atoi(argv[1]);
-	if (value < 0)
+	cents = atoi(argv[1]);
+	if (cents < 0)
 	{
 		printf("%d\n", 0);
 		return (0);
 	}
-	if (value % 25 >= 0)
+	if (cents % 25 >= 0)
 	{
-		c += value / 25;
-		value = value % 25;
+		change = cents / 25;
+		cents = cents % 25;
 	}
-	if (value % 10 >= 0)
+	if (cents % 10 >= 0)
 	{
-		c += value / 10;
-		value = value % 10;
+		change += cents / 10;
+		cents = cents % 10;
 	}
-	if (value % 5 >= 0)
+	if (cents % 5 >= 0)
 	{
-		c += value / 5;
-		value = value % 5;
+		change += cents / 5;
+		cents = cents % 5;
 	}
-	if (value % 2 >= 0)
+	if (cents % 2 >= 0)
 	{
-		c += value / 2;
-		value = value % 2;
+		change += cents / 2;
+		cents = cents % 2;
 	}
-	if (value % 1 >= 0)
+	if (cents % 1 >= 0)
 	{
-		c += value / 1;
+		change += cents / 1;
 	}
-	printf("%d\n", c);
-	return (0)
+	printf("%d\n", change);
+	return (0);
 }
